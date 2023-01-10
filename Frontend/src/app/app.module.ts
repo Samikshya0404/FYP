962d6ae9f35bdb38a897d37b9116cb5072367ee7
unknown <gsamikshya0@gmail.com>
2023-01-10 17:18:18 +0545

    registration form validation

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -40,7 +40,9 @@ import { BlogdetailsComponent } from './blogdetails/blogdetails.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  private ngUnsubscribe: Subject<any> = new Subject();
+  registerForm!: FormGroup;
 
+  constructor(private formbuilder: FormBuilder) {
+    this.registerForm = this.formbuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
+      username: ['', Validators.required],
+      password1: ['', Validators.required],
+      gender: ['', Validators.required],
+      password2: ['', Validators.required],
+      phone : ['',Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    console.log('submitted !!')
+  }
 }
